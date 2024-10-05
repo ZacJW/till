c08a2f4a799561e5b0ea095028be2d8ef5ae830f
ZacJW <zac@zacjw.com>
2024-10-05 18:23:00 +0100

    Make Blocking generic over context type

--- till-blocking/src/lib.rs
+++ till-blocking/src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 
+pub mod maybe_explicit;
 pub mod maybe_send;
 
 use core::{
@@ -7,39 +8,20 @@ use core::{
     pin::{pin, Pin},
 };
 
+use maybe_explicit::{ExplicitContext, ImplicitContext};
 use maybe_send::Satisfies;
 
-// use maybe_send::{Assert, Holds};
-
 /// Executor support for spawning blocking functions on a thread that won't block async tasks
 /// from making progress.
-pub trait BlockingExplicit<SendBound: maybe_send::SendBound> {
+pub trait Blocking<SendBound: maybe_send::SendBound, Context: maybe_explicit::MaybeExplicit<Self>> {
     type Node<T: Satisfies<SendBound> + 'static>: BlockingNode<Output = Result<T, Self::Error>>;
     type Error;
 
-    fn spawn_blocking_explicit<
+    fn spawn_blocking<
         T: Satisfies<SendBound> + 'static,
         F: FnOnce() -> T + Satisfies<SendBound> + 'static,
     >(
-        &self,
-        node: Pin<&mut Self::Node<T>>,
-        f: F,
-    );
-}
-
-/// Executor support for spawning blocking functions on a thread that won't block async tasks
-/// from making progress.
-///
-/// Unlike [Blocking], this trait uses an implicit context mechanism to register the blocking
-/// function with the executor.
-pub trait BlockingImplicit<SendBound: maybe_send::SendBound> {
-    type Node<T: Satisfies<SendBound> + 'static>: BlockingNode<Output = Result<T, Self::Error>>;
-    type Error;
-
-    fn spawn_blocking_implicit<
-        T: Satisfies<SendBound> + 'static,
-        F: FnOnce() -> T + Satisfies<SendBound> + 'static,
-    >(
+        context: Context,
         node: Pin<&mut Self::Node<T>>,
         f: F,
     );
@@ -50,16 +32,17 @@ pub trait BlockingNode: Future {
 }
 
 pub async fn spawn_blocking_explicit<
+    'a,
     SendBound: maybe_send::SendBound,
     T: Satisfies<SendBound> + 'static,
     F: FnOnce() -> T + Satisfies<SendBound> + 'static,
-    B: BlockingExplicit<SendBound>,
+    B: Blocking<SendBound, ExplicitContext<'a, B>>,
 >(
-    blocking: &B,
+    context: &'a B,
     f: F,
-) -> Result<T, <B as BlockingExplicit<SendBound>>::Error> {
+) -> Result<T, <B as Blocking<SendBound, ExplicitContext<'a, B>>>::Error> {
     let mut node = pin!(B::Node::<T>::new_empty());
-    blocking.spawn_blocking_explicit(node.as_mut(), f);
+    B::spawn_blocking(context.into(), node.as_mut(), f);
     node.await
 }
 
@@ -67,12 +50,12 @@ pub async fn spawn_blocking_implicit<
     SendBound: maybe_send::SendBound,
     T: Satisfies<SendBound> + 'static,
     F: FnOnce() -> T + Satisfies<SendBound> + 'static,
-    B: BlockingImplicit<SendBound>,
+    B: Blocking<SendBound, ImplicitContext<B>>,
 >(
     f: F,
-) -> Result<T, <B as BlockingImplicit<SendBound>>::Error> {
+) -> Result<T, <B as Blocking<SendBound, ImplicitContext<B>>>::Error> {
     let mut node = pin!(B::Node::<T>::new_empty());
-    B::spawn_blocking_implicit(node.as_mut(), f);
+    B::spawn_blocking(Default::default(), node.as_mut(), f);
     node.await
 }
 
@@ -80,34 +63,22 @@ pub async fn spawn_blocking_implicit<
 /// from making progress.
 ///
 /// Unlike [Blocking], this trait allow functions to be spawned in an eager way (i.e. before the first await of the join handle)
-pub trait EagerBlockingExplicit<SendBound: maybe_send::SendBound> {
+pub trait EagerBlocking<
+    SendBound: maybe_send::SendBound,
+    Context: maybe_explicit::MaybeExplicit<Self>,
+>
+{
     type Handle<T>: EagerBlockingHandle<Return = T>;
 
-    fn spawn_eager_blocking_explicit<
+    fn spawn_eager_blocking<
         T: Satisfies<SendBound> + 'static,
         F: FnOnce() -> T + Satisfies<SendBound> + 'static,
     >(
-        &self,
+        context: Context,
         f: F,
     ) -> Self::Handle<T>;
 }
 
 pub trait EagerBlockingHandle {
     type Return;
-}
-
-/// Executor support for spawning blocking functions on a thread that won't block async tasks
-/// from making progress.
-///
-/// Unlike [EagerBlocking], this trait uses an implicit context mechanism to register the blocking
-/// function with the executor.
-pub trait EagerBlockingImplicit<SendBound: maybe_send::SendBound> {
-    type Handle<T>: EagerBlockingHandle<Return = T>;
-
-    fn spawn_eager_blocking_implicit<
-        T: Satisfies<SendBound> + 'static,
-        F: FnOnce() -> T + Satisfies<SendBound> + 'static,
-    >(
-        f: F,
-    ) -> Self::Handle<T>;
 }
